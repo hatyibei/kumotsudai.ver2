222a1fe7f1394acf1495e95cc9d2dafb4cfbb667
hatyibei <hatyibei7@yahoo.co.jp>
2025-08-06 07:02:35 +0900

    amplify展開

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "kumotsudai7b200372": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
